e7f90c8ef328a64519ac70d66f81d414d69a9dca
Manoj Vivek <p.manoj.vivek@gmail.com>
2019-06-10 09:49:46 +0530

    Exporting components

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Prefetcher from "./prefetcher";
+
+export default Prefetcher;
+
+export const OnLoadPrefetcher = ({ link, ...other }) => (
+  <Prefetcher renderLink={link} {...other} />
+);
+
+export const OnHoverPrefetcher = ({ link, ...other }) => (
+  <Prefetcher hoverLink={link} {...other} />
+);
+
+export const OnClickPrefetcher = ({ link, ...other }) => (
+  <Prefetcher clickLink={link} {...other} />
+);
--- src/prefetcher.js
+++ src/prefetcher.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
 
 export default class Prefetcher extends React.Component {
   constructor(props) {
@@ -6,31 +6,35 @@ export default class Prefetcher extends React.Component {
     this.state = {
       rendered: false,
       hovered: false,
-      clicked: false,
-    }
+      clicked: false
+    };
   }
 
   _delay = this.props.delayMs || 500;
 
-  _eventHappened = event => setTimeout(() => this.setState({[event + 'ed']: true}), this._delay);
+  _eventHappened = event =>
+    setTimeout(() => this.setState({ [event + "ed"]: true }), this._delay);
 
   componentDidMount() {
-    this._eventHappened('render');
+    this._eventHappened("render");
   }
 
   render() {
     return (
       <Fragment>
-        <div style={{width: 'max-content'}} onMouseEnter={() => this._eventHappened('hover')} onClick={() => this._eventHappened('click')}>{this.props.children}</div>
+        <div
+          style={{ width: "max-content" }}
+          onMouseEnter={() => this._eventHappened("hover")}
+          onClick={() => this._eventHappened("click")}
+        >
+          {this.props.children}
+        </div>
         {this.state.rendered && this._renderLink(this.props.renderLink)}
         {this.state.hovered && this._renderLink(this.props.hoverLink)}
         {this.state.clicked && this._renderLink(this.props.clickLink)}
       </Fragment>
-    )
+    );
   }
 
-  _renderLink = href => (
-    href && <link rel="prefetch" href={href} />
-  );
-
+  _renderLink = href => href && <link rel="prefetch" href={href} />;
 }
